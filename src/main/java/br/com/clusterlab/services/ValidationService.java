e9010957f4e13a3149bd24e40caefeaafe73235b
rocko83 <alexandre@rocko83.com>
2023-02-08 11:32:06 -0300

    replaced admission reviewe with dto

--- src/main/java/br/com/clusterlab/services/ValidationService.java
+++ src/main/java/br/com/clusterlab/services/ValidationService.java
@@ -3,6 +3,8 @@ package br.com.clusterlab.services;
 import br.com.clusterlab.dto.response.AdmissionReview;
 import br.com.clusterlab.dto.response.Response;
 import br.com.clusterlab.dto.response.Status;
+import br.com.clusterlab.dto.validation.pod.Container;
+import br.com.clusterlab.dto.validation.pod.PodAdmissionReview;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -15,21 +17,18 @@ import org.springframework.web.bind.annotation.*;
 public class ValidationService {
     Logger logger = LoggerFactory.getLogger(ValidationService.class);
     @PostMapping({"/pods"})
-    public String pods(@RequestBody ObjectNode request) {
+    public String pods(@RequestBody PodAdmissionReview podAdmissionReview) throws JsonProcessingException {
         ObjectMapper om = new ObjectMapper();
-        String uid = request.path("request").required("uid").asText();
+        String uid = podAdmissionReview.getRequest().getUid();
         Status status = new Status();
         Response response = new Response();
         AdmissionReview admissionReview = new AdmissionReview();
-        String resourceMatch = request.path("request").path("resource").required("resource").textValue().toString().toLowerCase();
-        logger.info(resourceMatch);
+        String resourceMatch = podAdmissionReview.getRequest().getResource().getResource();
         if ( ! "pods".equals(resourceMatch)) {
-            logger.info(resourceMatch + "not iqual " + "pods");
             status.setCode(403);
             status.setMessage("Resource NOT Authorized.\"");
             response.setAllowed(false);
         } else {
-            logger.info(resourceMatch + " iqual " + "pods");
             status.setCode(200);
             status.setMessage("Resource Authorized");
             response.setAllowed(true);
@@ -45,7 +44,15 @@ public class ValidationService {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        logger.info(request.toString());
+        for (Container container : podAdmissionReview.getRequest().getObject().getSpec().getContainers())
+        {
+          logger.info(container.getName());
+        }
+        logger.info(
+                "UID=" + podAdmissionReview.getRequest().getUid() +
+                " Namespace=" + podAdmissionReview.getRequest().getNamespace() +
+                " Name=" + podAdmissionReview.getRequest().getName() +
+                " Operation=" + podAdmissionReview.getRequest().getOperation());
         logger.info("RESPONSEDATA " + responseData);
         return responseData;
     }
